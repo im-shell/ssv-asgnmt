80e92213f84d689a63ff803e189017bdb98a3ae5
Sameer Kumar <hellosam1010@gmail.com>
2025-08-30 17:33:12 +0530

    update: added Subscription struct to manage eash subscriptions

---
+++ src/interfaces/ISubscriptionService.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+interface ISubscriptionService {
+    ////////////////////////
+    ///// STRUCTS /////
+    ////////////////////////
+
+    /**
+     * 1. Check if a subscriber ID is subscribed to a provider ID: mapping(uint256 subscriberId => mapping(uint256
+     * providerId => Subscription)) providerSubscribers;
+     *     2.
+     */
+    struct Provider {
+        address owner;
+        uint256 fee;
+        bool isActive;
+    }
+
+    struct Subscriber {
+        address owner;
+        uint256 currentBalance;
+        uint256 totalDeposits;
+        Provider[] providers;
+    }
+
+    struct Subscription {
+        uint48 lastBillingTs;
+        bool paused;
+    }
+
+    ////////////////////////
+    ///// EVENTS /////
+    ////////////////////////
+
+    event ProviderRegistered(uint256 providerId, address owner, uint256 fee);
+    event SubscriberRegistered(uint256 subscriberId, address owner, uint256 depositAmt);
+    event ProviderUnregistered(uint256 providerId, address owner);
+    event SubscriberUnregistered(uint256 subscriberId, address owner);
+    event Subscribed(uint256 providerId, uint256 subscriberId);
+    event Unsubscribed(uint256 providerId, uint256 subscriberId);
+
+    ////////////////////////
+    ///// ERRORS /////
+    ////////////////////////
+
+    error RegistrationKeyAlreadyUsed(bytes32 key);
+    error InvalidProviderFee(uint256 fee);
+    error MaxProvidersReached();
+    error InvalidSubscriberDeposit(uint256 deposit);
+    error SubscriberAlreadyRegistered(address owner);
+    error ProviderNotRegistered(uint256 providerId);
+    error ProviderAlreadyRegistered(address owner);
+    error InsufficientDeposit(uint256 deposit, uint256 requiredDeposit);
+    error ProviderNotActive(uint256 providerId);
+    error Unauthorized();
+
+    ////////////////////////
+    ///// EXTERNAL API /////
+    ////////////////////////
+
+    function registerProvider(bytes32 key, uint256 fee) external returns (uint256);
+    function registerSubscriber(uint256[] memory providers, uint256 depositAmt) external returns (uint256);
+    // function increaseDeposit(uint256 amt) external;
+    function unregisterProvider(uint256 providerId) external;
+    function deactivateProvider(uint256 providerId) external;
+    function subscribe(uint256 providerId, uint256 depositAmt) external;
+    function unsubscribe(uint256 providerId) external;
+    function collectEarnings() external;
+
+    // Getters
+    function getProvider(uint256 providerId) external view returns (uint256, uint256, bool);
+    function getSubscriber(uint256 subscriberId) external view returns (uint256, uint256, uint256, bool);
+    function getProviderBalance(uint256 providerId) external view returns (uint256);
+    function getProviderEarnings(uint256 providerId) external view returns (uint256);
+    function getSubscriberBalance(uint256 subscriberId) external view returns (uint256);
+    function getSubscriberDepositValueUSD(uint256 subscriberId) external view returns (uint256);
+}
